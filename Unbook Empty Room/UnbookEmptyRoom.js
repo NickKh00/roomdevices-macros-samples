60f1487153e51bcc5e0421523c4784e5b37d2c8d
NickKh00 <95952634+NickKh00@users.noreply.github.com>
2023-05-26 14:29:48 +0800

    Update UnbookEmptyRoom.js

--- Unbook Empty Room/UnbookEmptyRoom.js
+++ Unbook Empty Room/UnbookEmptyRoom.js
@@ -77,201 +77,8 @@
  let bookingId;
  let meetingId;
 
- class PresenceDetector {
-     /* * Presence detector class handling all the logic * to detect presence on Cisco equipment */
-     constructor() {
-         this._data = {
-             peopleCount: 0,
-             peoplePresence: false,
-             inCall: false,
-             presenceSound: false,
-             sharing: false,
-         };
-         // flags for full/empty room timers
-         this._lastFullTimer = 0;
-         this._lastEmptyTimer = 0;
-         this._roomIsFull = false;
-         this._roomIsEmpty = false;
-     }
-
-     async enableDetector() {
-         /*
-          * Enabling detection on Cisco equipment
-          */
-         console.log("Enabling presence detection...");
-         if (!USE_ULTRASOUND) {
-             console.log("Ultrasound detection disabled!");
-         }
-         if (!USE_SOUND) {
-             console.log("Sound detection disabled!");
-         } else {
-             console.log("Sound detection enabled!");
-         }
-         xapi.config.set('HttpClient Mode', 'On');
-         xapi.config.set('RoomAnalytics PeopleCountOutOfCall', 'On');
-         xapi.config.set('RoomAnalytics PeoplePresenceDetector', 'On');
-         console.log("Success, presence detection enabled");
-     }
-
-     _getData(command) {
-         return xapi.status.get(command).catch((error) => {
-             console.warning("Couldn't run the command:", command, " Error: ", error);
-             return -1
-         })
-     }
-     _isRoomOccupied() {
-         /*
-          * Logic that returns a boolean if room is occupied combining all types of enabled presence detection
-          */
-         if (!USE_PRESENCE_AND_COUNT) {
-             console.log("# of people:" + this._data.peopleCount + "| Ultrasound presence detected: " + this._data.peoplePresence + "| Call in progress: " + this._data.inCall + "| Sound level above " + SOUND_LEVEL + ": " + this._data.presenceSound + "| Sharing (Sending or Receiving): " + this._data.sharing);
-             console.log("IS OCCUPIED: " + (this._data.peopleCount || (USE_ULTRASOUND && this._data.peoplePresence) || (USE_ACTIVE_CALLS && this._data.inCall) || (USE_SOUND && this._data.presenceSound) || (USE_PRESENTATION_MODE && this._data.sharing)));
-
-             return this._data.peopleCount || (USE_ULTRASOUND && this._data.peoplePresence) || (USE_ACTIVE_CALLS && this._data.inCall) || (USE_SOUND && this._data.presenceSound) || (USE_PRESENTATION_MODE && this._data.sharing);
-         } else {
-             //if USE_PRESENCE_AND_COUNT is true
-             console.log("Presence and face detected: " + (this._data.peoplePresence && this._data.peopleCount) + "| Call in progress: " + this._data.inCall + "| Sound level above " + SOUND_LEVEL + ": " + this._data.presenceSound + "| Sharing (Sending or Receiving): " + this._data.sharing);
-             console.log("IS OCCUPIED: " + (this._data.peopleCount && this._data.peoplePresence) || (USE_ACTIVE_CALLS && this._data.inCall) || (USE_SOUND && this._data.presenceSound) || (USE_PRESENTATION_MODE && this._data.sharing));
-
-             return (this._data.peopleCount && this._data.peoplePresence) || (USE_ACTIVE_CALLS && this._data.inCall) || (USE_SOUND && this._data.presenceSound) || (USE_PRESENTATION_MODE && this._data.sharing);
-         }
-     }
-     _processPresence() {
-         /*
-          *  Logic for the presence information gathered from
-          *  the Cisco equipment.
-          */
-         if (this._isRoomOccupied()) {
-             if (this._lastFullTimer == 0) {
-                 console.log("Room occupancy detected - starting timer...");
-                 this._lastFullTimer = Date.now();
-                 this._lastEmptyTimer = 0;
-             } else if (Date.now() > (this._lastFullTimer + MIN_BEFORE_BOOK * 60000)) {
-                 this._roomIsFull = true;
-                 this._roomIsEmpty = false;
-                 this._lastFullTimer = Date.now();
-                 console.log("Should update room status - occupied");
-             }
-         } else {
-             if (this._lastEmptyTimer == 0) {
-                 console.log("Room empty detected - starting timer...");
-                 this._lastEmptyTimer = Date.now();
-                 this._lastFullTimer = 0;
-             } else if (Date.now() > (this._lastEmptyTimer + MIN_BEFORE_RELEASE * 60000) && !this._roomIsEmpty) {
-                 this._roomIsFull = false;
-                 this._roomIsEmpty = true;
-                 console.log("Should update room status - empty");
-             }
-         }
-
-         if (this._roomIsEmpty) {
-             if (listenerShouldCheck) {
-                 listenerShouldCheck = false;
-                 console.warn("Room is empty start countdown for delete booking");
-                 this._startCountdown();
-             }
-         }
-     }
-
-     _startCountdown() {
-         console.log("No presence Detected");
-         xapi.command("UserInterface Message Prompt Display", {
-             Text: "This room seems unused. It will be self-released.<br>Press check-in if you have booked this room",
-             FeedbackId: 'alert_response',
-             'Option.1': 'CHECK IN',
-         }).catch((error) => {
-             console.error(error);
-         });
-         alertDuration = 60;
-         refreshInterval = setInterval(updateEverySecond, 1000);
-         delete_timeout = setTimeout(() => {
-             console.log("No presence Detected so the booking has been removed from this device");
-             xapi.Command.UserInterface.Message.Prompt.Clear({
-                 FeedbackId: "alert_response"
-             });
-             xapi.Command.UserInterface.Message.TextLine.Clear({});
-             clearInterval(forcedUpdate);
-
-             //here we get the updeted meetingId to decline the meeting
-             xapi.Command.Bookings.Get({
-                 Id: bookingId
-             }).then(book => {
-                 xapi.Command.Bookings.Respond({
-                     Type: "Decline",
-                     MeetingId: book.Booking.MeetingId
-                 });
-             });
-
-             bookingId = null;
-             bookingIsActive = false;
-             this._lastFullTimer = 0;
-             this._lastEmptyTimer = 0;
-             this._roomIsFull = false;
-             this._roomIsEmpty = false;
-         }, 60000);
-
-     }
-
-     _checkPresenceAndProcess() {
-         this._processPresence();
-     }
-
-     async updatePresence() {
-         /*
-          * Polling the Cisco information
-          * and getting presence information
-          */
-         const callsData = this._getData('SystemUnit State NumberOfActiveCalls');
-         const presenceData = this._getData('RoomAnalytics PeoplePresence');
-         const peopleCountData = this._getData('RoomAnalytics PeopleCount Current');
-         const soundData = this._getData('RoomAnalytics Sound Level A');
-         const presentationData = this._getData('Conference Presentation Mode');
-
-         Promise.all([callsData, presenceData, peopleCountData, soundData, presentationData]).then(results => {
-             const numCalls = parseInt(results[0]);
-             const presence = results[1] === 'Yes' ? true : false;
-             const peopleCount = parseInt(results[2]);
-             const soundLevel = parseInt(results[3]);
-             const presentationMode = results[4] === 'Off' ? false : true;
-             if (!USE_PRESENTATION_MODE) {
-                 this._data.sharing = false;
-             } else {
-                 this._data.sharing = presentationMode;
-             }
-             this._data.peopleCount = peopleCount === -1 ? 0 : peopleCount;
-             this._data.peoplePresence = presence;
-             if (!USE_ULTRASOUND) {
-                 // if ultrasound is disabled we set people presence
-                 // based only of image reconigition
-                 this._data.peoplePresence = this._data.peopleCount > 0 ? true : false;
-             }
-             // process conference calls
-             if (numCalls > 0 && USE_ACTIVE_CALLS) {
-                 this._data.inCall = true;
-                 this._data.peoplePresence = true;
-                 // if in call we set that people are present
-             } else {
-                 this._data.inCall = false;
-             }
-
-             //process sound level
-             if ((soundLevel > SOUND_LEVEL) && USE_SOUND) {
-                 this._data.presenceSound = true;
-             } else {
-                 this._data.presenceSound = false;
-             }
-             this._processPresence();
-         });
-     }
- }
-
-
  async function beginDetection() {
      /* * Entry point for the macro */
-
-     const presence = new PresenceDetector();
-     await presence.enableDetector(); // to configure Cisco equipment to detect presence informations
-
      //when meeting starts
      xapi.Event.Bookings.Start.on(async booking_info => {
          console.log("Booking " + booking_info.Id + " detected");
@@ -288,7 +95,7 @@
 
                      //each minutes it check if _checkPresenceAndProcess should be called (in order to declare a room empty or full).
                      //This is done because the update depends on listeners and if no events are detected the method will never be called
-                     forcedUpdate = setInterval(() => {
+                     /*forcedUpdate = setInterval(() => {
 
                          if (presence._isRoomOccupied()) {
                              if ((presence._lastFullTimer != 0)) {
@@ -306,15 +113,15 @@
                              }
                          }
 
-                     }, (MIN_BEFORE_BOOK * 60000) + 1000);
+                     }, (MIN_BEFORE_BOOK * 60000) + 1000);*/
                  });
              } else {
                  bookingId = null;
                  meetingId = null;
-                 presence._lastFullTimer = 0;
+                 /*presence._lastFullTimer = 0;
                  presence._lastEmptyTimer = 0;
                  presence._roomIsFull = false;
-                 presence._roomIsEmpty = false;
+                 presence._roomIsEmpty = false;*/
                  console.log("Booking was detected but shouldn't have been");
              }
          });
@@ -332,16 +139,16 @@
          listenerShouldCheck = false;
          bookingId = null;
          meetingId = null;
-         presence._lastFullTimer = 0;
+         /*presence._lastFullTimer = 0;
          presence._lastEmptyTimer = 0;
          presence._roomIsFull = false;
-         presence._roomIsEmpty = false;
+         presence._roomIsEmpty = false;*/
          console.log("Booking " + booking_info.Id + " ended Stop Checking");
      });
 
 
      //Active Call
-     xapi.Status.SystemUnit.State.NumberOfActiveCalls.on(numberOfcall => {
+     /*xapi.Status.SystemUnit.State.NumberOfActiveCalls.on(numberOfcall => {
          if (bookingIsActive) {
              console.log("Number of active call: " + numberOfcall);
              if (parseInt(numberOfcall) > 0 && USE_ACTIVE_CALLS) {
@@ -355,10 +162,10 @@
                  presence._checkPresenceAndProcess();
              }
          }
-     });
+     });*/
 
      //Presence
-     xapi.Status.RoomAnalytics.PeoplePresence.on(presenceValue => {
+     /*xapi.Status.RoomAnalytics.PeoplePresence.on(presenceValue => {
          if (bookingIsActive) {
              console.log("Presence: " + presenceValue);
              presenceValue = presenceValue === 'Yes' ? true : false;
@@ -385,11 +192,11 @@
                  presence._checkPresenceAndProcess();
              }
          }
-     });
+     });*/
 
 
      //People Count
-     xapi.Status.RoomAnalytics.PeopleCount.Current.on(nb_people => {
+     /*xapi.Status.RoomAnalytics.PeopleCount.Current.on(nb_people => {
          if (bookingIsActive) {
              console.log("People count: " + nb_people);
              nb_people = parseInt(nb_people);
@@ -420,11 +227,11 @@
                  presence._checkPresenceAndProcess();
              }
          }
-     });
+     });*/
 
 
      //Sound Level
-     xapi.Status.RoomAnalytics.Sound.Level.A.on(level => {
+     /*xapi.Status.RoomAnalytics.Sound.Level.A.on(level => {
          if (bookingIsActive) {
              console.log("Sound level: " + level);
              level = parseInt(level);
@@ -437,11 +244,11 @@
                  presence._checkPresenceAndProcess();
              }
          }
-     });
+     });*/
 
 
      //Presentation Mode (Off/Receiving/Sending)
-     xapi.Status.Conference.Presentation.Mode.on(mode => {
+     /*xapi.Status.Conference.Presentation.Mode.on(mode => {
          if (bookingIsActive) {
              console.log("Presentation Mode: " + mode);
              mode = mode === 'Off' ? false : true;
@@ -454,10 +261,10 @@
                  presence._checkPresenceAndProcess();
              }
          }
-     });
+     });*/
 
      //GUI Interaction
-     xapi.Event.UserInterface.Extensions.on(event => {
+     /*xapi.Event.UserInterface.Extensions.on(event => {
          if (bookingIsActive && USE_GUI_INTERACTION) {
              console.log("Touch Panel interaction detected");
              xapi.Command.UserInterface.Message.Prompt.Clear({
@@ -476,7 +283,7 @@
                  presence._checkPresenceAndProcess();
              }
          }
-     });
+     });*/
 
 
      xapi.event.on('UserInterface Message Prompt Response', (event) => {
@@ -489,9 +296,9 @@
                          clearInterval(refreshInterval);
                          xapi.Command.UserInterface.Message.TextLine.Clear({});
                          listenerShouldCheck = true;
-                         presence._data.peoplePresence = true;
+                         /*presence._data.peoplePresence = true;
                          presence._roomIsEmpty = false;
-                         presence._roomIsFull = true;
+                         presence._roomIsFull = true;*/
                          break;
                      default:
                          break;
